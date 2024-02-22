7dea4f41f8b7f6ead7c0bfe5f81439feeb39bda8
mpan <Matti.Panula@seamk.fi>
2024-02-22 17:57:48 +0200

    canvas test

--- data_structures.py
+++
@@ -1,7 +0,0 @@
-class MySingleStringHolder:
-    def __init__(self, string):
-        self.string = string
-    def get(self):
-        return self.string
-    def set(self, string):
-        self.string = string

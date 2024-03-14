805063587e64a6cc031537b01124709f87f79c03
mpan <Matti.Panula@seamk.fi>
2024-03-14 17:16:52 +0200

    Revert "Revert "canvas test""
    This reverts commit 1bbdb68f28ecdc58a758302aeb3b0b20106d660c.

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, jsonify
 from data_structures import MySingleStringHolder
 import html_helper as h
 
 my_data = MySingleStringHolder("Hello World")
+circle_coordinates = {"x": 100, "y": 100, "radius": 50}
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
 def root_get():
-    html_body = h.form(h.form_name) + "<br>My data: " + my_data.get()
+    html_body = h.form(h.form_name) + "<br>Circle: " + str(circle_coordinates) + "<br>"
+    html_body += open('canvas.html', 'r').read()
     html = h.root("Otsikko", html_body)
     return html
 
 @app.route("/", methods = ["POST"])
 def root_post():    
-    my_data.set(request.form[h.form_name])
+    coords = request.form[h.form_name].split(",")
+    global circle_coordinates
+    circle_coordinates = {"x": int(coords[0]), "y": int(coords[1]), "radius": int(coords[2])}
+    
     return redirect("/")
+
+@app.route("/circle-coordinates", methods = ["GET"])
+def circle_get():    
+    return jsonify(circle_coordinates)
 
 if __name__ == "__main__":
     app.run()

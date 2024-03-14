c87de33638a82b813e6c3d997d9842b419ec44e1
mpan <Matti.Panula@seamk.fi>
2024-03-14 20:01:53 +0200

    some canvas examples added

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -10,7 +10,32 @@ fetch('/circle-coordinates')
 
 // Function to draw a circle
 function drawCircle(x, y, radius) {
+  ctx.strokeStyle = "rgb(0, 0, 0)";
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.stroke();
 }
+
+ctx.font = "48px serif";
+ctx.fillText("Hello world", 10, 50);
+
+datalista = [0,2, 0.5, 0.8, 0.3]
+
+start_x = 300
+start_y = 200
+width = 300
+height = 100
+
+ctx.beginPath();
+// y-axis
+ctx.lineTo(start_x, start_y);
+ctx.lineTo(start_x, start_y + height);
+// x-axis
+ctx.moveTo(start_x, start_y + height)
+ctx.lineTo(start_x, start_y + height);
+ctx.lineTo(start_x + width, start_y + height);
+
+ctx.stroke();
+
+
+
